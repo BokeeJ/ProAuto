12fdbaa14b9239f0440459d9a3a5c5798fe279b7
BokeeJ <bokeejusthard@gmail.com>
2024-05-02 12:47:07 +0200

    Repair Email

--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -63,7 +63,7 @@ function Kontakt() {
                         <hr className='w-[35%] border-red-800 border-4 
                     mt-2' />
                     </div>
-                    <a href={`tel:${telefonBroj}`} className='text-white text-4xl flex' target='_blank'><FaPhoneAlt size={35} className='mr-4' />064 94 95 922</a>
+                    <a href={`tel:${telefonBroj}`} aria-label="Call on icon" className='text-white text-4xl flex' target='_blank'><FaPhoneAlt size={35} className='mr-4' />064 94 95 922</a>
                     <div className='mt-10'>
                         <h2 className='text-white text-3xl font-serif font-bold'>Adresa:</h2>
                         <p className='text-white text-xl'>Svetozara Markovica 1 <br />Lazarevac</p>
@@ -82,12 +82,9 @@ function Kontakt() {
                     className='lg:w-[50%] h-full w-[80%] bg-black rounded-2xl opacity-80'>
                     <h1 className='text-white font-bold text-4xl text-center p-10'>POŠALJI NAM EMAIL</h1>
                     <form className='flex flex-col gap-5  p-10' ref={form} onSubmit={sendEmail}>
-                        <label className='text-red-800 text-2xl'>Name</label>
-                        <input className='text-xl h-10 p-1' type="text" name="user_name" />
-                        <label className='text-red-800 text-2xl'>Email</label>
-                        <input className='text-xl h-10 p-1' type="email" name="user_email" />
-                        <label className='text-red-800 text-2xl'>Message</label>
-                        <textarea className='w-full h-[120px] p-1' name="message" />
+                        <input className='text-xl h-10 p-1' placeholder='Ime' type="text" name="user_name" />
+                        <input className='text-xl h-10 p-1' placeholder='E-mail' type="email" name="user_email" />
+                        <textarea className='w-full h-[120px] p-1' placeholder='Poruka' name="message" />
                         <button className='text-2xl text-red-800 border-white border-2 rounded-3xl p-3 font-bold font-mono' type="submit" value="Send">Send</button>
                     </form>
                 </motion.div>
